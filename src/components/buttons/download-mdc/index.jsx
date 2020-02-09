0cda44cf0497f3cf4f1a0a2edfd9ae4ba784ae84
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2020-02-09 13:00:59 +0100

    chg: removed pdf files from repo

--- src/components/buttons/download-mdc/index.jsx
+++ src/components/buttons/download-mdc/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-
 import Button from '@material-ui/core/Button';
 
-// import axios from 'axios';
+import ReactDOM from 'react-dom';
 
 import { StoreContext } from '../../datastore-context';
 import { PrinterContext } from '../../printer-context';
@@ -20,23 +18,8 @@ export default function DownloadMDCButton() {
     getDocuments
   } = React.useContext(PrinterContext);
 
-  const downloadMDC = () => {
-    // Save it so the JSON is on the server
-    saveMDC(setWindowToDownloadPath, true, false);
-  };
-
-  const setWindowToDownloadPath = (key) => {
-    window.location = `${baseURL}download?kb=${key}`;
-  };
-
-  const saveMDC = async (cb, new_id = false, update_id = true) => {
+  const downloadMDC = async () => {
     const data = {};
-
-    // Get some key. TODO: Seems to not be working as intended - only returns undefined
-    const key = getKey();
-    if (key && !new_id) {
-      data['key'] = key;
-    }
 
     // Define the fields we want, then for each field...
     [
@@ -75,40 +58,27 @@ export default function DownloadMDCButton() {
     });
 
     // TODO: Turn the data into HTML
-    const createHTML = (data) => {
-      // console.log('Data:', data)
-      return `<div class="person">
-           <h2>This is test!!!!</h2>
-           <ul>
-           ${data.waypoints.map((wp, index) => {
-              console.log(wp)
-             return <li>{wp[1]}</li>;
-           })}
-           </ul>
-        </div>
-      `;
+    const createHTML = ({ waypoints }) => {
+      console.log('Data:', data);
+
+      const renderedWaypoints = data.waypoints
+        .map((waypoint) => `<li>${waypoint.wp}</ li>`)
+        .join('');
+
+      return `<div class="person"><h2>Waypoints:</h2><ul>${renderedWaypoints}</ul></div>`;
     };
 
-    const dogs = [
-      { name: 'Snickers', age: 2 },
-      { name: 'Hugo', age: 8 },
-      { name: 'Sunny', age: 1 }
-    ];
-
-    const markup = `<ul class="dogs">
-        ${dogs.map((dog) => `<li>${dog.name} is ${dog.age * 7}</li>`)}
-      </ul>`;
-
-    console.log(markup);
-    console.log(ReactDOMServer.renderToString(markup));
-
-    console.log(createHTML(data))
-
     // Build the API payload
-    // let payload = {
-    //   name: 'TEST DATA',
-    //   document: createHTML(data)
-    // };
+    let payload = {
+      name: 'TEST DATA',
+      document: createHTML(data)
+    };
+
+    console.log(createHTML(data));
+
+    const foo = <div>Testing Testing</div>
+
+    ReactDOM.render(foo)
 
     // Send the payload to the backend
     // addDocument(payload).then(({status, data}) => {
@@ -129,26 +99,6 @@ export default function DownloadMDCButton() {
     //     deleteDocument(doc.id)
     //   })
     // })
-  };
-
-  // TODO: this seems to not be working. Only getting 'undefined'
-  const getKey = () => {
-    // Key is either with a # or the end path
-    // so if the end path matches the uuid, use that, else look for hash
-
-    var uuid_regex = /(?:^#|.*\/)(([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})|([a-zA-Z0-9]{8}))$/;
-
-    var match = uuid_regex.exec(window.location);
-    if (match && match[1]) {
-      return match[1];
-    }
-
-    match = uuid_regex.exec(window.location.hash);
-    if (match && match[1]) {
-      return match[1];
-    }
-
-    return undefined;
   };
 
   return (
