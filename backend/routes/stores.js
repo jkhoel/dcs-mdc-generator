d4e0197a0a37c18d6c94477a07612522ee42c447
Jens Kristian Hoel <github@hoelweb.com>
2020-02-18 19:16:07 +0100

    chg: supports now show correctly on default template

--- backend/routes/stores.js
+++ backend/routes/stores.js
@@ -1,41 +1,41 @@
 const express = require('express');
-const fs = require('fs');
-var parser = require('xml2json');
+// const fs = require('fs');
+// var parser = require('xml2json');
 const router = express.Router();
 
-const xmlfile_stores = __dirname + '/../data/cf/stores.xml';
-const xmlfile_aircraft = __dirname + '/../data/cf/aircraft.xml';
+// const xmlfile_stores = __dirname + '/../data/cf/stores.xml';
+// const xmlfile_aircraft = __dirname + '/../data/cf/aircraft.xml';
 
-router.get('/', async (req, res, next) =>
-  fs.readFile(xmlfile_stores, 'utf-8', (err, text) => {
-    if (err) throw err;
+// router.get('/', async (req, res, next) =>
+//   fs.readFile(xmlfile_stores, 'utf-8', (err, text) => {
+//     if (err) throw err;
 
-    let data = parser.toJson(text);
-    res.send(JSON.parse(data)['Stores']['Store']); // Sends the Store Array
-  })
-);
+//     let data = parser.toJson(text);
+//     res.send(JSON.parse(data)['Stores']['Store']); // Sends the Store Array
+//   })
+// );
 
-router.get('/aircraft', async (req, res, next) =>
-  fs.readFile(xmlfile_aircraft, 'utf-8', (err, text) => {
-    if (err) throw err;
+// router.get('/aircraft', async (req, res, next) =>
+//   fs.readFile(xmlfile_aircraft, 'utf-8', (err, text) => {
+//     if (err) throw err;
 
-    let str = parser.toJson(text);
-    let data = JSON.parse(str);
+//     let str = parser.toJson(text);
+//     let data = JSON.parse(str);
 
-    console.log(req.params);
+//     console.log(req.params);
 
-    // If we have an aircraft id parameter - return only that aircraft
-    // if (req.params.id) {
-    let ac = data['AircraftList']['Aircraft'].filter(
-      aircraft => aircraft.Name === 'F-16C_50'
-    );
+//     // If we have an aircraft id parameter - return only that aircraft
+//     // if (req.params.id) {
+//     let ac = data['AircraftList']['Aircraft'].filter(
+//       aircraft => aircraft.Name === 'F-16C_50'
+//     );
 
-    res.send(ac);
-    // } else {
-    // If not, return all
-    //  res.send(data['AircraftList']['Aircraft']);
-    // }
-  })
-);
+//     res.send(ac);
+//     // } else {
+//     // If not, return all
+//     //  res.send(data['AircraftList']['Aircraft']);
+//     // }
+//   })
+// );
 
 module.exports = router;
